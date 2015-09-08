ebf19e8a08cd2296620544bd6d8556d1584dca8b
David Sheffield <david.sheffield21@gmail.com>
2015-09-07 21:54:27 -0700

    adding app.js and test.js to complete data structure assingment

---
+++ app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var sports = ['football', 'baseball', 'basketball'];
+var moreSports = ['soccer', 'hockey']
+
+// array push
+Array.prototype.push.apply(sports, moreSports);
+console.log(sports);
+// array pop
+Array.prototype.pop.apply(sports);
+console.log(sports);
+// array shift
+Array.prototype.shift(sports);
+console.log(sports);
+// array unshift
+Array.prototype.unshift.call(sports, 'football');
+console.log(sports);
+
+
+// unique function
+var unique = function(array) {
+  var seen = {};
+  var current;
+  var newArray = [];
+
+  for(var i = 0; i < array.length; i++) {
+      current = array[i];
+      if(!seen[current]) {
+        seen[current] = true;
+        newArray.push(current);
+      }
+  }
+  return newArray;
+};
+unique();
+
+
+// frequency 2 function
+var frequencyTwo = function(array){
+  var seen = {};
+  var current;
+  var maxCount = 0;
+  var maxLetter = [];
+
+  for (var i = 0; i < array.length; i++) {
+      current = arrayString.charAt(i);
+      if (!seen[current]) {
+          seen[current] = 1;
+      } else {
+          current++;
+      }
+      if (seen[current] > maxCount) {
+        maxCount = seen[current];
+        maxLetter = [maxCount];
+      }
+      if (seen[current] === maxCount) {
+        maxLetter.push(current);
+      }
+  }
+  return maxLetter.slice(1, maxLetter.length);
+}
+frequencyTwo();
+console.log(frequencyTwo(array));
+
+
+exports.unique = unique;
+exports.frequencyTwo = frequencyTwo;
