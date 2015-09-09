10c03c0596b8979400556d742c7ab15d436c5615
David Sheffield <david.sheffield21@gmail.com>
2015-09-08 17:04:26 -0700

    updating tests and changing app.js to array_methods.js

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
 'use strict';
 
-var app = require(__dirname + '/../app');
+var arrayMeth = require(__dirname + '/../array_methods');
 var expect = require('chai').expect;
 
 
 describe('unique', function() {
    it('should return an array with any duplicate removed', function() {
       var testArray = [1, 2, 3, 2, 5];
-      expect(app.unique(testArray)).to.eql([1, 2, 3, 5]);
+      expect(arrayMeth.unique(testArray)).to.eql([1, 2, 3, 5]);
    });
 });
 
 describe('frequencyTwo', function() {
    it('should find the letter most used in the English language', function() {
       var testArray = ['david, dude, add, daddy'];
-      expect(app.frequencyTwo(testArray)).to.eql(['d']);
+      expect(arrayMeth.frequencyTwo(testArray)).to.eql(['d']);
    });
 });
